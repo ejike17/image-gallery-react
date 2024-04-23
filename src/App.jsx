fd44d6fcd3efa03dd2d2dd427e37ea06b0664ee4
Ejike John <ejikeanumba17@gmail.com>
2024-04-23 03:45:08 +0100

    Image gallery in React JS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ImageSlider from "./components";
+
+function App() {
+  
+  return (
+    <div className="App">
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={"5"} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.jsx
@@ -0,0 +1,67 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import "./style.css"
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+
+export default function ImageSlider({ url, page = 1, limit = 5 }) {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setcurrentSlide] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchImages(getUrl) {
+    try {
+      const response = await fetch(`${getUrl}?${page}=1&limit=${limit}`);
+      const data = await response.json();
+
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (error) {
+      setErrorMsg(error);
+    }
+  }
+
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  }, [url]);
+
+  if (loading) {
+    return <div>Loading data please wait</div>;
+  }
+
+  if (errorMsg !== null) {
+    return <div>Error Occurred! {errorMsg}</div>;
+  }
+
+  function handlePrev() {
+    setcurrentSlide(currentSlide === 0 ?  images.length-1 : currentSlide-1)
+  }
+
+  function handleNext() {
+    setcurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1)
+  }
+
+  return (
+    <div className="container">
+      <BsArrowLeftCircleFill onClick={handlePrev} className="arrow arrow-left" />
+      {images && images.length
+        ? images.map((imageItem, index) => (
+          <img key={imageItem.id} alt={imageItem.download_url} src={imageItem.download_url}
+            className={ currentSlide === index ? "current-image" : "current-image hide-current-image"} />)
+          )
+        : null}
+      <BsArrowRightCircleFill onClick={handleNext} className="arrow arrow-right" />
+      <span className="circle-indicators">
+        {
+          images && images.length ? images.map((_, index) => <button
+            key={index}
+            className={currentSlide === index ? "current-indicator" : "inactive-indicator"}
+            onClick={()=>setcurrentSlide(index)}
+          ></button>) : null
+        }
+      </span>
+    </div>
+  );
+}
